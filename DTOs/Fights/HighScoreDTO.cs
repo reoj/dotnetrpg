9d1efec7b85c29e434f0d053c716821842a2b100
Jaciel Israel Resendiz Ochoa <jresendizochoa@deloitte.com>
2022-10-12 07:28:06 -0500

    Properties for High Score

--- DTOs/Fights/HighScoreDTO.cs
+++ DTOs/Fights/HighScoreDTO.cs
@@ -2,6 +2,10 @@ namespace dotnetrpg.DTOs.Fights
 {
     public class HighScoreDTO
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Fights { get; set; }
+        public int Victories { get; set; }
+        public int Defeats { get; set; }
     }
 }
